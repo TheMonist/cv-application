154cf2d38eef4e568bb1ca9c5506b7ee8c8c0f2d
TheMonist <rogerrosena@gmail.com>
2024-01-17 20:03:03 -0500

    adding preview and skill section

---
+++ src/components/PreviewContainer.jsx
@@ -0,0 +1,129 @@
+import Button from "./Button";;
+// import "../styles//PreviewContainer.css";
+
+function PreviewContainer({
+    dialogRef,
+    closeDialog,
+    profile,
+    downloadPdf,
+  }) {
+    return (
+      <>
+        <dialog className="preview" ref={dialogRef}>
+          <div className="preview-buttons">
+            <Button
+              label="download"
+              className="preview-download"
+              onClick={downloadPdf}
+            />
+            <Button
+              label="close"
+              className="preview-close"
+              onClick={closeDialog}
+            />
+          </div>
+  
+          <div className="cv">
+            <div className="contact-section">
+              <p className="name">{profile.name}</p>
+              <p className="role">{profile.role}</p>
+              <p className="phone">{profile.phone}</p>
+              <p className="email">{profile.email}</p>
+              <p className="linkedin">{profile.linkedin}</p>
+              <p className="github">{profile.github}</p>
+            </div>
+            <div className="linebreak"></div>
+            <div className="summary-section">
+              <p>01 Professional Summary</p>
+              {profile.summary && (
+                <div
+                  className="summary"
+                  dangerouslySetInnerHTML={{
+                    __html: profile.summary.replace(/\n/g, "<br>"),
+                  }}
+                />
+              )}
+            </div>
+            <div className="linebreak"></div>
+            <div className="experience-section">
+              <p>02 Job Experience</p>
+              {profile.experiences.map((experience, index) => (
+                <div key={experience.id} className="experience">
+                  <div className="dates">
+                    <div className="experience-from">From: {experience.from}</div>
+                    <div className="experience-to">To: {experience.to}</div>
+                  </div>
+                  <div className="experience-details">
+                    <div className="experience-title-company-location">
+                      <div className="experience-title">{experience.title}</div>
+                      <div className="experience-company">
+                        {experience.company}
+                      </div>
+                      <div className="experience-location">
+                        {experience.location}
+                      </div>
+                    </div>
+                    <div
+                      className="responsibilities"
+                      dangerouslySetInnerHTML={{
+                        __html: experience.responsibilities.replace(
+                          /\n/g,
+                          "<br>"
+                        ),
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="linebreak"></div>
+            <div className="education-section">
+              <p>03 Education</p>
+              {profile.educations.map((education, index) => (
+                <div key={education.id} className="education">
+                  <div className="dates">
+                    <div className="education-from">From: {education.from}</div>
+                    <div className="education-to">To: {education.to}</div>
+                  </div>
+                  <div className="education-details">
+                    <div className="education-degree-school-location">
+                      <div className="education-degree">{education.degree}</div>
+                      <div className="education-school">{education.school}</div>
+                      <div className="education-location">
+                        {education.location}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="linebreak"></div>
+            <div className="skill-section">
+              <p>04 Skills</p>
+              <div className="skills">
+                {profile.skills.map((skill, index) => (
+                  <div key={skill.id} className="skill">
+                    {skill.name}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="linebreak"></div>
+            <div className="language-section">
+              <p>05 Languages</p>
+              <div className="languages">
+                {" "}
+                {profile.languages.map((language, index) => (
+                  <div key={language.id} className="language">
+                    {language.name}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </dialog>
+      </>
+    );
+  }
+
+  export default PreviewContainer;
---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,55 @@
+import Input from "./Input";
+import Button from "./Button";
+// import "..styles/SkillsSection.css";
+
+function SkillsSection({
+    skils,
+    addItem,
+    removeItem,
+    onChange
+}) {
+    function addSkill() {
+        const newSkill = {
+            id: Math.random(),
+            name: ""
+        };
+        addItem("skills", newSkill);
+    }
+
+    return (
+        <div className="skill-section">
+            <p>04 Skills</p>
+            <div className="skills">
+                {skills.map((skill, index) => (
+                    <div key={skill.id}>
+                        <Input
+                            label="Skill"
+                            placeholder="Skill"
+                            className="skill-name"
+                            value={skill.name}
+                            onChange={(value) => 
+                                onChange(index, "name", value, "skills"
+                            )}
+                        />
+                        <Button 
+                            label="o"
+                            className="true"
+                            onClick={() => {
+                                removeItem("skills", skill)
+                            }}
+                        />
+                    </div>
+                ))}
+            </div>
+            <Button 
+                label="Add skill"
+                className="add-skill"
+                onClick={() => {
+                    addSkill();
+                }}
+            />
+        </div>
+    );
+}
+
+export default SkillsSection;
